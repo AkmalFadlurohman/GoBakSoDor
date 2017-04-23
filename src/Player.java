43cf47585cfead0b9061a242a22c0b02a2806ad8
dikiardian <ardiandiki369@gmail.com>
2017-04-23 17:55:37 +0700

    player and enemy

--- src/Player.java
+++ src/Player.java
@@ -2,10 +2,38 @@
  * Created by Diki Ardian W (13515092) on 4/23/17.
  */
 public class Player implements Movable{
+  private String name;
+  private int score;
+  private int life;
+
   public Player() {
-    
+
   }
   public void move() {
 
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  public void setScore(int score) {
+    this.score = score;
+  }
+
+  public int getLife() {
+    return life;
+  }
+
+  public void setLife(int life) {
+    this.life = life;
   }
 }
