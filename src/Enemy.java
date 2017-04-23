f41490174a821b4d98e51c3bcb2f2830ac22f31c
um <fahrurrozi31@gmail.com>
2017-04-23 17:18:25 +0700

    nambain enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,7 +2,21 @@
  * Created by Diki Ardian W (13515092) on 4/23/17.
  */
 public class Enemy implements Movable{
-  public void move() {
-    asdsad
+
+  int akuLah;
+  final String e;
+
+  public Enemy(String e) {
+    this.e = e;
+  }
+
+  public void moveEnemy() {
+
+  }
+  public void an() {
+
+  }
+  public void qwww(){
+
   }
 }
