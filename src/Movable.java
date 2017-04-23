fa7f8fca4cf3bdff29fc7ea824b6862c986aaa8f
dikiardian <ardiandiki369@gmail.com>
2017-04-23 17:00:54 +0700

    coba dulu

---
+++ src/Movable.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Diki Ardian W (13515092) on 4/23/17.
+ */
+public interface Movable {
+  void move();
+}
---
+++ src/Player.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Diki Ardian W (13515092) on 4/23/17.
+ */
+public class Player implements Movable{
+  public void move() {
+
+  }
+}
