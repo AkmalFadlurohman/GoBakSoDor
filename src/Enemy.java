f2b7dd88027b61052abb28c0e435e5a9f1a26597
um <fahrurrozi31@gmail.com>
2017-04-23 17:36:40 +0700

    ini sesuatu

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,22 +1,11 @@
 /**
  * Created by Diki Ardian W (13515092) on 4/23/17.
  */
-public class Enemy implements Movable{
+public class Enemy implements Movable {
 
-  int akuLah;
-  final String e;
 
-  public Enemy(String e) {
-    this.e = e;
-  }
-
-  public void moveEnemy() {
-
-  }
-  public void an() {
-
-  }
-  public void qwww(){
+  @Override
+  public void move() {
 
   }
 }
