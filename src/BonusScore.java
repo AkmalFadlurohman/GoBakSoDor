65a8b86eb0ba9d2b041505a7efe46726a7ad33dc
freedomfeather <fadlurohmanakmal@rocketmail.com>
2017-04-23 19:52:21 +0700

    add bonus,heart,special

---
+++ src/BonusScore.java
@@ -0,0 +1,13 @@
+/**
+ * Created by akmalfadlurohman on 4/23/17.
+ */
+public class BonusScore extends Item {
+  public BonusScore() {
+    super("Bonus Score","Player's score +10",0,0);
+  }
+  public void applyEffect() {
+  }
+  public void applyEffect(Player P) {
+    P.setScore(P.getScore()+10);
+  }
+}
---
+++ src/Heart.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by akmalfadlurohman on 4/23/17.
+ */
+public class Heart {
+}
