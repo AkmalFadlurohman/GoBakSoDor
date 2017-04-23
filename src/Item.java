0acc720c33cadbbb1b2d4a52eb5712b30cd85f19
freedomfeather <fadlurohmanakmal@rocketmail.com>
2017-04-23 17:31:22 +0700

    add item

---
+++ src/Item.java
@@ -0,0 +1,13 @@
+/**
+ * Created by akmalfadlurohman on 4/23/17.
+ */
+public class Item {
+  private String effect;
+
+  public Item(String e) {
+    effect = e;
+  }
+  public String getEffect() {
+    return effect;
+  }
+}
